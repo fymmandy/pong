1ec157f98f31a7368f6ca5ea1a73878ea5284db1
fymmandy <fongymmandy@gmail.com>
2023-09-19 17:20:05 +0100

    fixed the paddle collision issue

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ BLACK = (0,0,0)
 #ball (radius in pixel, ball coordinates)
 radius = 15
 ball_x, ball_y = WIDTH/2 - radius,  HEIGHT/2 - radius
-ball_vel_x, ball_vel_y = 0.5,0.5
+ball_vel_x, ball_vel_y = 0.2,0.2
 
 #Paddle dimensions
 paddle_width, paddle_height = 20, 120
@@ -42,49 +42,49 @@ while run:
         if i.type == pygame.KEYUP:
               right_paddle_vel = 0
               left_paddle_vel = 0
-        #BALL'S MOVEMENT CONTROL
-        if (ball_y <= 0 + radius) or (ball_y >= HEIGHT - radius):
-            ball_vel_y*= -1
-        if ball_x >= WIDTH -radius:
-            ball_x, ball_y = WIDTH/2 - radius,  HEIGHT/2 - radius
-            ball_vel_x, ball_val_y = 0.5,0.5
+    #BALL'S MOVEMENT CONTROL
+    if (ball_y <= 0 + radius) or (ball_y >= HEIGHT - radius):
+        ball_vel_y*= -1
+    if ball_x >= WIDTH -radius:
+        ball_x, ball_y = WIDTH/2 - radius,  HEIGHT/2 - radius
+        ball_vel_x, ball_val_y = 0.2,0.2
+        ball_vel_x *= -1
+    if ball_x <=0 + radius:
+        ball_x, ball_y = WIDTH/2 - radius,  HEIGHT/2 - radius
+        ball_vel_x, ball_vel_y = 0.2,0.2
+        
+    #paddle's movement control
+    if left_paddle_y >= HEIGHT - paddle_height:
+        left_paddle_y = HEIGHT - paddle_height
+    if left_paddle_y <= 0:
+        left_paddle_y = 0
+    if right_paddle_y >= HEIGHT - paddle_height:
+        right_paddle_y = HEIGHT - paddle_height
+    if right_paddle_y <= 0:
+        right_paddle_y = 0
+    
+    #paddle collisions
+    #left paddle collisons
+    #check if x position of ball is within the range of the paddle's x position
+    if left_paddle_x <= ball_x <= left_paddle_x + paddle_width:
+        if left_paddle_y <= ball_y <= left_paddle_y + paddle_height:
+            ball_x = left_paddle_x + paddle_width
             ball_vel_x *= -1
-        if ball_x <=0 + radius:
-            ball_x, ball_y = WIDTH/2 - radius,  HEIGHT/2 - radius
-            ball_vel_x, ball_vel_y = 0.5,0.5
-            
-        #paddle's movement control
-        if left_paddle_y >= HEIGHT - paddle_height:
-            left_paddle_y = HEIGHT - paddle_height
-        if left_paddle_y <= 0:
-            left_paddle_y = 0
-        if right_paddle_y >= HEIGHT - paddle_height:
-            right_paddle_y = HEIGHT - paddle_height
-        if right_paddle_y <= 0:
-            right_paddle_y = 0
-        
-        #paddle collisions
-        #left paddle collisons
-        #check if x position of ball is within the range of the paddle's x position
-        if left_paddle_x <= ball_x <= left_paddle_x + paddle_width:
-            if left_paddle_y <= ball_y <= left_paddle_y <= paddle_height:
-                ball_x = left_paddle_x + paddle_width
-                ball_vel_x *= -1
-        #right paddle collisions
-        if right_paddle_x <= ball_x <= right_paddle_x + paddle_width:
-            if right_paddle_y <= ball_y <= right_paddle_y <= paddle_height:
-                ball_x = right_paddle_x
-                ball_vel_x *= -1
+    #right paddle collisions
+    if right_paddle_x <= ball_x <= right_paddle_x + paddle_width:
+        if right_paddle_y <= ball_y <= right_paddle_y + paddle_height:
+            ball_x = right_paddle_x
+            ball_vel_x *= -1
                 
-        #movement
-        ball_x += ball_vel_x
-        ball_y += ball_vel_y
-        right_paddle_y += right_paddle_vel
-        left_paddle_y += left_paddle_vel
-        
-        #objects      
-        pygame.draw.circle(wn, BLUE, (ball_x, ball_y),radius)
-        pygame.draw.rect(wn, RED, pygame.Rect(left_paddle_x, left_paddle_y, paddle_width, paddle_height))
-        pygame.draw.rect(wn, RED, pygame.Rect(right_paddle_x, right_paddle_y, paddle_width, paddle_height))
+    #movement
+    ball_x += ball_vel_x
+    ball_y += ball_vel_y
+    right_paddle_y += right_paddle_vel
+    left_paddle_y += left_paddle_vel
+    
+    #objects      
+    pygame.draw.circle(wn, BLUE, (ball_x, ball_y),radius)
+    pygame.draw.rect(wn, RED, pygame.Rect(left_paddle_x, left_paddle_y, paddle_width, paddle_height))
+    pygame.draw.rect(wn, RED, pygame.Rect(right_paddle_x, right_paddle_y, paddle_width, paddle_height))
 
-        pygame.display.update()
+    pygame.display.update()
