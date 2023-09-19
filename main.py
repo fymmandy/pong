f047abbc04b414227524620af1d08a0abef7d332
fymmandy <fongymmandy@gmail.com>
2023-09-19 15:47:34 +0100

    fixed ball repeat issue

--- main.py
+++ main.py
@@ -11,22 +11,57 @@ run = True
 #colours in rgb format
 BLUE = (0,0,255)
 RED = (255,0,0)
+BLACK = (0,0,0)
 
 #ball (radius in pixel, ball coordinates)
 radius = 15
 ball_x, ball_y = WIDTH/2 - radius,  HEIGHT/2 - radius
+ball_vel_x, ball_vel_y = 0.3,0.3
 
 #Paddle dimensions
 paddle_width, paddle_height = 20, 120
 left_paddle_y = right_paddle_y = HEIGHT/2 - paddle_height/2
 left_paddle_x, right_paddle_x = 100 - paddle_width/2, WIDTH-(100 - paddle_width/2)
+right_paddle_vel = left_paddle_vel = 0
+
 
 #main loop
 while run:
+    wn.fill(BLACK)
     for i in pygame.event.get():
         if i.type ==pygame.QUIT:
             run = False
-            
+        elif i.type ==pygame.KEYDOWN:
+            if i.key == pygame.K_UP:
+                right_paddle_vel =-0.4
+            if i.key == pygame.K_DOWN:
+                right_paddle_vel = 0.4 
+            if i.key == pygame.K_w:
+                left_paddle_vel =-0.4
+            if i.key == pygame.K_s:
+                left_paddle_vel = 0.4  
+        if i.type == pygame.KEYUP:
+              right_paddle_vel = 0
+              left_paddle_vel = 0
+    #BALL'S MOVEMENT CONTROL
+    if ball_y <= 0 + radius or ball_y >= HEIGHT - radius:
+        ball_vel_y*= -1
+    if ball_x >= WIDTH -radius:
+        ball_x, ball_y = WIDTH/2 - radius,  HEIGHT/2 - radius
+        ball_vel_x *= -1
+        ball_vel_y *= -1
+    if ball_x <=0 + radius:
+        ball_x, ball_y = WIDTH/2 - radius,  HEIGHT/2 - radius
+        ball_vel_x, ball_vel_y = 0.3,0.3
+        
+    #paddle's movement control
+    
+    #movement
+    ball_x += ball_vel_x
+    ball_y += ball_vel_y
+    right_paddle_y += right_paddle_vel
+    left_paddle_y += left_paddle_vel
+    #objects      
     pygame.draw.circle(wn, BLUE, (ball_x, ball_y),radius)
     pygame.draw.rect(wn, RED, pygame.Rect(left_paddle_x, left_paddle_y, paddle_width, paddle_height))
     pygame.draw.rect(wn, RED, pygame.Rect(right_paddle_x, right_paddle_y, paddle_width, paddle_height))
