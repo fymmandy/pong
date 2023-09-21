2b466c3dfff272aaee7203e919ce7a23ec934e38
fymmandy <fongymmandy@gmail.com>
2023-09-21 17:45:34 +0100

    added phantom ball

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ import random
 
 pygame.init()
 
+gadget_pair = 1
+ch = int(input("Enter your choice for gadget pair: "))
+if ch == 1:
+    gadget_pair = 1
+elif ch == 2:
+    gadget_pair = 2
 #initials
 WIDTH, HEIGHT = 1000, 600
 wn = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -22,7 +28,10 @@ radius = 15
 ball_x, ball_y = WIDTH/2 - radius,  HEIGHT/2 - radius
 ball_set_x, ball_set_y = 0.3, 0.3
 ball_vel_x, ball_vel_y = 0.2,0.2
-
+#creation of phantom ball
+phantom_ball_x, phantom_ball_y = WIDTH/2 - radius,  HEIGHT/2 - radius
+phantom_ball_set_x, phantom_ball_set_y = 0.3, 0.3
+phantom_ball_vel_x, phantom_ball_vel_y = 0.2,0.2
 #Paddle dimensions
 paddle_width, paddle_height = 20, 120
 left_paddle_y = right_paddle_y = HEIGHT/2 - paddle_height/2
@@ -63,44 +72,63 @@ while run:
     #BALL'S MOVEMENT CONTROL
     if (ball_y <= 0 + radius) or (ball_y >= HEIGHT - radius):
         ball_vel_y*= -1
+    if (phantom_ball_y <= 0 + radius) or (phantom_ball_y >= HEIGHT - radius):
+        phantom_ball_vel_y*= -1
     if ball_x >= WIDTH - radius:
         ball_x, ball_y = WIDTH/2 - radius,  HEIGHT/2 - radius
+        phantom_ball_x, phantom_ball_y = WIDTH/2 - radius,  HEIGHT/2 - radius
         dir = random.choice(direction)
         ang = random.choice(angle)
         if dir == 0:
             if ang == 0:
                 ball_vel_y, ball_vel_x = -2*ball_set_y, ball_set_x
+                phantom_ball_vel_y, phantom_ball_vel_x = -2*phantom_ball_set_y, phantom_ball_set_x
             if ang == 1:
                 ball_vel_y,ball_vel_x = -1*ball_set_y, ball_set_x
+                phantom_ball_vel_y,phantom_ball_vel_x = -1*phantom_ball_set_y, phantom_ball_set_x
             if ang == 2:
                 ball_vel_y,ball_vel_x = -1*ball_set_y, 2*ball_set_x
+                phantom_ball_vel_y,phantom_ball_vel_x = -1*phantom_ball_set_y, 2*phantom_ball_set_x
         if dir == 1:
             if ang == 0:
                 ball_vel_y, ball_vel_x = 2*ball_set_y, ball_set_x
+                phantom_ball_vel_y, phantom_ball_vel_x = 2*phantom_ball_set_y, phantom_ball_set_x
             if ang == 1:
                 ball_vel_y,ball_vel_x = 1*ball_set_y, ball_set_x
+                phantom_ball_vel_y,phantom_ball_vel_x = 1*phantom_ball_set_y, phantom_ball_set_x
             if ang == 2:
                 ball_vel_y,ball_vel_x = 1*ball_set_y, 2*ball_set_x
-        ball_vel_x *= -1
+                phantom_ball_vel_y,phantom_ball_vel_x = 1*phantom_ball_set_y, 2*phantom_ball_set_x
 
+        ball_vel_x *= -1
+        phantom_ball_vel_x *= -1
+        
     if ball_x <=0 + radius:
         ball_x, ball_y = WIDTH/2 - radius,  HEIGHT/2 - radius
+        phantom_ball_x, phantom_ball_y = WIDTH/2 - radius,  HEIGHT/2 - radius
         dir = random.choice(direction)
         ang = random.choice(angle)
         if dir == 0:
             if ang == 0:
                 ball_vel_y, ball_vel_x = -2*ball_set_y, ball_set_x
+                phantom_ball_vel_y, phantom_ball_vel_x = -2*phantom_ball_set_y, phantom_ball_set_x
             if ang == 1:
                 ball_vel_y,ball_vel_x = -1*ball_set_y, ball_set_x
+                phantom_ball_vel_y,phantom_ball_vel_x = -1*phantom_ball_set_y, phantom_ball_set_x
             if ang == 2:
                 ball_vel_y,ball_vel_x = -1*ball_set_y, 2*ball_set_x
+                phantom_ball_vel_y,phantom_ball_vel_x = -1*phantom_ball_set_y, 2*phantom_ball_set_x
         if dir == 1:
             if ang == 0:
-                ball_vel_y, ball_vel_x = 2*ball_set_y, ball_set_x
+                ball_vel_y,ball_vel_x = 2*ball_set_y, ball_set_x
+                phantom_ball_vel_y,phantom_ball_vel_x = 2*phantom_ball_set_y, phantom_ball_set_x
             if ang == 1:
                 ball_vel_y,ball_vel_x = 1*ball_set_y, ball_set_x
+                phantom_ball_vel_y,phantom_ball_vel_x = 1*phantom_ball_set_y, phantom_ball_set_x
             if ang == 2:
                 ball_vel_y,ball_vel_x = 1*ball_set_y, 2*ball_set_x
+                phantom_ball_vel_y,phantom_ball_vel_x = 1*phantom_ball_set_y, 2*phantom_ball_set_x
+
 
         
     #paddle's movement control
@@ -119,43 +147,75 @@ while run:
     if left_paddle_x <= ball_x <= left_paddle_x + paddle_width:
         if left_paddle_y <= ball_y <= left_paddle_y + paddle_height:
             ball_x = left_paddle_x + paddle_width
+            phantom_ball_x = left_paddle_x + paddle_width
             ball_vel_x *= -1
+            phantom_ball_vel_x *= -1
     #right paddle collisions
     if right_paddle_x <= ball_x <= right_paddle_x + paddle_width:
         if right_paddle_y <= ball_y <= right_paddle_y + paddle_height:
             ball_x = right_paddle_x
+            phantom_ball_x = right_paddle_x
             ball_vel_x *= -1
-    
+            phantom_ball_vel_x *= -1
+
     #gadgets in action
     # gadget 1 is slam
-    if left_gadget == 1:
-        if left_paddle_x <= ball_x <= left_paddle_x + paddle_width:
-            if left_paddle_y <= ball_y <= left_paddle_y + paddle_height:
-                ball_x = left_paddle_x + paddle_width
-                ball_vel_x *= -3
-                left_gadget = 0
-                left_gadget_remaining -= 1
-    #gadget 2 is flash
-    elif left_gadget == 2:
-        left_paddle_y = ball_y
-        left_gadget = 0
-        left_gadget_remaining -= 1 
-            
-    if right_gadget == 1:
-        if right_paddle_x <= ball_x <= right_paddle_x + paddle_width:
-            if right_paddle_y <= ball_y <= right_paddle_y + paddle_height:
-                ball_x = right_paddle_x
-                ball_vel_x *= -3
-                right_gadget = 0
-                right_gadget_remaining -= 1
-    elif right_gadget == 2:
-        right_paddle_y = ball_y
-        right_gadget = 0
-        right_gadget_remaining -= 1
+    if gadget_pair == 1:
+        if left_gadget == 1:
+            if left_paddle_x <= ball_x <= left_paddle_x + paddle_width:
+                if left_paddle_y <= ball_y <= left_paddle_y + paddle_height:
+                    ball_x = left_paddle_x + paddle_width
+                    ball_vel_x *= -3
+                    phantom_ball_vel_x *= -3
+                    left_gadget = 0
+                    left_gadget_remaining -= 1
+        #gadget 2 is flash
+        elif left_gadget == 2:
+            left_paddle_y = ball_y
+            left_gadget = 0
+            left_gadget_remaining -= 1 
+                
+        if right_gadget == 1:
+            if right_paddle_x <= ball_x <= right_paddle_x + paddle_width:
+                if right_paddle_y <= ball_y <= right_paddle_y + paddle_height:
+                    ball_x = right_paddle_x
+                    ball_vel_x *= -3
+                    phantom_ball_vel_x *= -3
+                    right_gadget = 0
+                    right_gadget_remaining -= 1
+        elif right_gadget == 2:
+            right_paddle_y = ball_y
+            right_gadget = 0
+            right_gadget_remaining -= 1
+    #second pair gadget
+    elif gadget_pair == 2:
+        if left_gadget == 1:
+            if left_paddle_x <= ball_x <= left_paddle_x + paddle_width:
+                if left_paddle_y <= ball_y <= left_paddle_y + paddle_height:
+                    ball_x = left_paddle_x + paddle_width
+                    phantom_ball_x = left_paddle_x + paddle_width
+                    ball_vel_x *= -1
+                    phantom_ball_vel_x *= -1
+                    phantom_ball_vel_y *= -1
+                    left_gadget = 0
+                    left_gadget_remaining -= 1
+        if right_gadget == 1:
+            if right_paddle_x <= ball_x <= right_paddle_x + paddle_width:
+                if right_paddle_y <= ball_y <= right_paddle_y + paddle_height:
+                    ball_x = right_paddle_x
+                    phantom_ball_x = right_paddle_x
+                    ball_vel_x *= -1
+                    phantom_ball_vel_x *= -1
+                    phantom_ball_vel_y *= -1
+                    right_gadget = 0
+                    right_gadget_remaining -= 1                    
+
         
     #movement
     ball_x += ball_vel_x
     ball_y += ball_vel_y
+    phantom_ball_x += phantom_ball_vel_x
+    phantom_ball_y += phantom_ball_vel_y
     right_paddle_y += right_paddle_vel
     left_paddle_y += left_paddle_vel
     
@@ -164,6 +224,9 @@ while run:
     pygame.draw.rect(wn, RED, pygame.Rect(left_paddle_x, left_paddle_y, paddle_width, paddle_height))
     pygame.draw.rect(wn, RED, pygame.Rect(right_paddle_x, right_paddle_y, paddle_width, paddle_height))
     
+    #phantom ball
+    pygame.draw.circle(wn, BLUE, (phantom_ball_x, phantom_ball_y),radius)
+
     #small circle with radius of 4 to indicate activated slam function
     if left_gadget == 1:
         pygame.draw.circle(wn, WHITE,(left_paddle_x + 10,left_paddle_y +10), 4)
